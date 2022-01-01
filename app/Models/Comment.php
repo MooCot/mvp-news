db4394c4f3fa85fd552f08ce6faab4590e2e8c1d
Slava Mora <gitmooncat@gmail.com>
2022-01-01 18:03:59 +0200

    create Post, Comment: Controller, model, migration, route

---
+++ app/Models/Comment.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
